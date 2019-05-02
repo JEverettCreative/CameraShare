edc434cb9dd9759f26b4859601e6ea4e25dbe969
JEverett <jonathan@jonathaneverettcreative.com>
2019-05-01 21:36:53 -0500

    Added schema and seeds file for establishing databases locally during development. Still need to add more dummy values.

---
+++ models/seeds.sql
@@ -0,0 +1,19 @@
+USE camerashare_db;
+
+INSERT INTO rentals (category, name, price, description)
+VALUES ("cameras", "Alexa Mini", 250, "Alexa Mini cinema camera, in excellent condition despite years of use.");
+
+INSERT INTO rentals (category, name, price, description)
+VALUES ("cameras", "Sony a6500", 25, "Highly functional and cost effective Sony mirrorless for stills or video.");
+
+INSERT INTO rentals (category, name, price, description)
+VALUES ("lenses", "Sigma 50mm", 110, "In good condition, despite some visual wear and tear.");
+
+INSERT INTO rentals (category, name, price, description)
+VALUES ("lenses", "Rokinon 75mm", 150, "Great lens for your next close up. Great condition.");
+
+INSERT INTO rentals (category, name, price, description)
+VALUES ("lighting", "Apurture 120d", 50, "Great, adaptable light. Comes with dimmer remote and stand.");
+
+INSERT INTO rentals (category, name, price, description)
+VALUES ("lighting", "Quasar Bicolor Package", 55, "Three 4ft bicolor Quasar LEDs. Range from 3200K to 5400K via dial. No accessories included.");
