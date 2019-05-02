2c3dee7d1d51d17634b309c8efe32ddb58a9dce2
JEverett <jonathan@jonathaneverettcreative.com>
2019-05-02 13:00:42 -0500

    Fleshed out the rest of the basic seeds for database.

--- models/seeds.sql
+++ models/seeds.sql
@@ -17,3 +17,30 @@ VALUES ("lighting", "Apurture 120d", 50, "Great, adaptable light. Comes with dim
 
 INSERT INTO rentals (category, name, price, description)
 VALUES ("lighting", "Quasar Bicolor Package", 55, "Three 4ft bicolor Quasar LEDs. Range from 3200K to 5400K via dial. No accessories included.");
+
+INSERT INTO rentals (category, name, price, description)
+VALUES ("stabilizers", "DJI Ronin MX-3", 65, "Lightweight motorized gimbal perfect for smoothing out shots on your next film. Includes all basic accessories.");
+
+INSERT INTO rentals (category, name, price, description)
+VALUES ("stabilizers", "Zhiyun Crane 2", 25, "Simple 3 axis gimbal. Great for DSLRs on low-impact shoots. Good condition.");
+
+INSERT INTO rentals (category, name, price, description)
+VALUES ("audio", "Zoom H4 Package", 75, "Everything you need for a simple audio setup: Zoom H4 recorder, 8ft boom, Rode mic with dead cat.");
+
+INSERT INTO rentals (category, name, price, description)
+VALUES ("audio", "Sennheiser EW Lav Kit", 80, "Two lavs with receivers, charger, and cables included.");
+
+INSERT INTO rentals (category, name, price, description)
+VALUES ("accessories", "Sony 50mm ND Filter Kit", 15, "Inclues 3 neutral density filters of varying strength for a 50mm lens, as well as one lens hood.");
+
+INSERT INTO rentals (category, name, price, description)
+VALUES ("accessories", "Manafrotto Tripod", 20, "Aluminum frame 62 inch tripod with fluid head and monopod transitional.");
+
+INSERT INTO users (username, hashstring)
+VALUES ("cowboybob23", "dkcuqknuihcneuakd");
+
+INSERT INTO users (username, hashstring)
+VALUES ("shutterspeedsally", "kjijcnkauhekjld");
+
+INSERT INTO users (username, hashstring)
+VALUES ("mysensorisbiggerthanyours18", "iwucsgsheucjsodedee");
