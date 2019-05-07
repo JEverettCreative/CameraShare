ea2dc7d33576ed963d9a4179bfb417b0f3ddf581
RedSW20 <kevbailey1227@gmail.com>
2019-05-06 21:16:06 -0500

    configuring google-passport

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,6 +2,17 @@ var db = require("../models");
 var path = require("path");
 module.exports = function(app) {
   // Load index page
+  app.get("/home", function(req, res) {
+    // db.Example.findAll({}).then(function(dbExamples) {
+    //   res.render("index", {
+    //     msg: "Welcome!",
+    //     examples: dbExamples
+    //   });
+    // });
+    res.sendFile(path.join(__dirname + "/../views/layouts/index.html"));
+  });
+
+  // Load leasing page
   app.get("/", function(req, res) {
     // db.Example.findAll({}).then(function(dbExamples) {
     //   res.render("index", {
