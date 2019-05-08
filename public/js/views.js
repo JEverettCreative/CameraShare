43974a0a2a09868c443cbaab516543a7a606e19a
JEverett <jonathan@jonathaneverettcreative.com>
2019-05-07 21:37:58 -0500

    Tidied up the page, wrapped form in a col centered and then haded a h3 over it. Toying with style choices now, including incorporating blue color to palette.

--- public/js/views.js
+++ public/js/views.js
@@ -56,8 +56,6 @@ $(document).on("click", ".item", function(event) {
   console.log("you got clicked");
 });
 
-// var submitButton = $("#why-button");
-// console.log('what is the button ===>> ::', submitButton);
 function acquireFormData() {
   return {
     price: $("#rental-price")
@@ -84,44 +82,9 @@ function postFormData(formData) {
     console.log("this is what we got back", data);
   });
 }
-$("#why-button").on("click", function(event) {
+$("#rental-submit").on("click", function(event) {
   event.preventDefault();
   var formData = acquireFormData();
   console.log(formData);
   postFormData(formData);
 });
-// function rendercategory(data) {
-//   if (data.length !== 0) {
-//     $("#stats").empty();
-//     $("#stats").show();
-
-//     for (var i = 0; i < data.length; i++) {
-//       var div = $("<div>");
-
-//       div.append("<h2>" + data[i].category + "</h2>");
-//       div.append("<p>name: " + data[i].name + "</p>");
-//       div.append("<p>price: " + data[i].price + "</p>");
-//       div.append("<p>description: " + data[i].description + "</p>");
-//       div.append(
-//         "<button class='delete' data-id='" +
-//           data[i].id +
-//           "'>DELETE category</button>"
-//       );
-
-//       $("#stats").append(div);
-//     }
-
-//     $(".delete").click(function() {
-//       $.ajax({
-//         method: "DELETE",
-//         url: "/api/category/" + $(this).attr("data-id")
-//       }).then(function() {
-//         console.log("Deleted Successfully!");
-//       });
-
-//       $(this)
-//         .closest("div")
-//         .remove();
-//     });
-//   }
-// }
